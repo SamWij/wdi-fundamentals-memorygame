baf58978648f6dda7bb28bcc2d5193a0126f70b8
Samantha Wijesinha <samanthawijesinha@Samanthas-MacBook-Pro.local>
2017-01-30 21:15:03 +1100

    added game logic function

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,22 +1,76 @@
 console.log("JS file is connected to HTML! Woo!");
-// var cardOne = "queen";
-// var cardTwo = "queen" ;
-// var cardThree = "king";
-// var cardFour = "king";
 
-/*if (cardOne === cardTwo) {
-alert("you found a match")
-} else if (cardOne === cardThree) {
-alert("Sorry Try Again")
-}*/
 
 var board = document.getElementById('game-board');
-function createCard() {
-	for (var x = 1; x <= 4; x += 1) {
-		var createDiv = document.createElement('div');
-		createDiv.className = 'card';
-		board.appendChild(createDiv);
-		console.log(createDiv);
+
+var cards = ['queen', 'queen', 'king', 'king', 'queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+var buttonPlayGame = document.getElementById ('playGame')
+
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i ++) {
+		var cardElement = document.createElement('div');
+		
+		cardElement.className = 'card';
+		
+		board.appendChild(cardElement);
+		
+		cardElement.setAttribute('data-card', cards[i]);
+		
+		cardElement.addEventListener('click', isTwoCards);
+		
+		console.log(cardElement);
 	}
 };
-createCard();
+
+
+
+function isMatch() {
+
+	if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
+
+
+
+function isTwoCards () {
+
+cardsInPlay.push(this.getAttribute('data-card'));
+	// show the card's image
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = '<img src="images/king.png">'; // king
+	} else {
+		this.innerHTML = '<img src="images/queen.png">'; //queen
+	}
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+    // clear cards in play array for next try
+    cardsInPlay = [];
+  }
+}
+
+createBoard(cards);
+
+
+
+
+buttonPlayGame.addEventListener('click', playGame);
+
+	function playGame() {
+
+		
+		window.location.reload();
+		
+	}
+
+
