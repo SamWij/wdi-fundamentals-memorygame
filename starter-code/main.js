2141317e79932e414efff6490d7b7891d3fe4f6a
Samantha Wijesinha <samanthawijesinha@Samanthas-MacBook-Pro.local>
2017-02-02 11:01:02 +1100

    final submission

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,76 +1,87 @@
 console.log("JS file is connected to HTML! Woo!");
 
-
 var board = document.getElementById('game-board');
 
-var cards = ['queen', 'queen', 'king', 'king', 'queen', 'queen', 'king', 'king'];
+var cards = ['queen', 'queen', 'king', 'king','queen', 'king'];
 
 var cardsInPlay = [];
 
-var buttonPlayGame = document.getElementById ('playGame')
+var buttonSingleGame = document.getElementById ('newGame')
 
-
+//create the board
 function createBoard() {
+
+
 	for (var i = 0; i < cards.length; i ++) {
 		var cardElement = document.createElement('div');
-		
+
 		cardElement.className = 'card';
-		
+
 		board.appendChild(cardElement);
-		
+
 		cardElement.setAttribute('data-card', cards[i]);
-		
+
 		cardElement.addEventListener('click', isTwoCards);
-		
+
 		console.log(cardElement);
+
 	}
+
 };
 
-
+createBoard(cards);
 
 function isMatch() {
 
-	if (cards[0] === cards[1]) {
+	if (cardsInPlay[0].charAt(0) === cardsInPlay[1].charAt(0)) {
     alert("You found a match!");
+
   } else {
     alert("Sorry, try again.");
 
   }
+
+	window.location.reload();
 }
 
+//randomize the array
+cards.sort(function() { return 0.5 - Math.random() });
 
 
+//display the cards clicked and display the image
 function isTwoCards () {
 
 cardsInPlay.push(this.getAttribute('data-card'));
-	// show the card's image
-	console.log(this.getAttribute('data-card'));
+
+console.log(this.getAttribute('data-card'));
+
 	if (this.getAttribute('data-card') === 'king') {
-		this.innerHTML = '<img src="images/king.png">'; // king
-	} else {
-		this.innerHTML = '<img src="images/queen.png">'; //queen
+
+		this.innerHTML = '<img src="images/king.png">';
+
+		} else {
+
+		this.innerHTML = '<img src="images/queen.png">';
 	}
-  // if you have two cards in play check for a match
-  if (cardsInPlay.length === 2) {
-    // pass the cardsInPlay as an argument to isMatch function
-    isMatch(cardsInPlay);
-    // clear cards in play array for next try
-    cardsInPlay = [];
-  }
+
+  	if (cardsInPlay.length === 2) {
+
+    	isMatch(cardsInPlay);
+
+    	cardsInPlay = [];
+  		}
+	}
+
+
+
+
+//on button click call newGame button which resets the board.
+buttonSingleGame.addEventListener('click', newGame);
+
+
+
+function newGame() {
+
+	window.location.reload();
+
 }
-
-createBoard(cards);
-
-
-
-
-buttonPlayGame.addEventListener('click', playGame);
-
-	function playGame() {
-
-		
-		window.location.reload();
-		
-	}
-
-
